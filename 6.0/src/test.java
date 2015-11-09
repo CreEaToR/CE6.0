0ad2617bf2f5c05923717061d046c8e463de3c15
CreEaToR <rockko10@gmail.com>
2015-11-09 12:20:37 -0500

    Initial

---
+++ 6.0/src/test.java
@@ -0,0 +1,12 @@
+
+public class test {
+
+	public static void main(String[] args) throws Exception{
+		
+		int arr[] = {2,4,5,6,8,7,5,5};
+		
+		System.out.println(Calculator.factorial(arr));
+
+	}
+
+}
